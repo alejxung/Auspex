84f9a672b0cc2a695c2f27f957d062b26f8c69c9
Alex Jung <60252636+alejxung@users.noreply.github.com>
2024-07-20 22:57:04 +0900

    Forecast Data

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title("Stock Prediction App")
 
-stocks = ("AAPL", "GOOG", "MSFT", "GME")
+stocks = ("AAPL", "GOOG", "MSFT", "NVDA")
 selected_stock = st.selectbox("Select dataset for prediction", stocks)
 
 n_years = st.slider("Years of prediction:", 1, 4)
@@ -38,3 +38,22 @@ def plot_raw_data():
   st.plotly_chart(fig)
 
 plot_raw_data()
+
+df_train = data[["Date", "Close"]]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+model = Prophet()
+model.fit(df_train)
+future = model.make_future_dataframe(periods=period)
+forecast = model.predict(future)
+
+st.subheader("Forecast data")
+st.write(forecast.tail())
+
+st.write("Forecast data")
+fig1 = plot_plotly(model, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = model.plot_components(forecast)
+st.write(fig2)
